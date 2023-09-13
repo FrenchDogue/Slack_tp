021330fc722f773d5c801bfff9416c8a1f6ac06c
Julie Thomas <ladyjulie63@gmail.com>
2023-09-13 17:22:10 +0200

    last one

--- slack_projet/src/app/canaux/pages/canal-discussion/canal-discussion.component.ts
+++ slack_projet/src/app/canaux/pages/canal-discussion/canal-discussion.component.ts
@@ -6,21 +6,20 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-canal-discussion',
   templateUrl: './canal-discussion.component.html',
-  styleUrls: ['./canal-discussion.component.scss']
+  styleUrls: ['./canal-discussion.component.scss'],
 })
 export class CanalDiscussionComponent {
-
   public canal!: Canal;
   public id!: number;
 
-  constructor(private activatedRoute : ActivatedRoute, private canauxMessagesService: CanauxMessagesService) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private canauxMessagesService: CanauxMessagesService
+  ) {
     this.id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
 
-    this.canauxMessagesService.getCanalById(this.id).subscribe(
-      (data) => (this.canal = data)
-    );
-
-    
+    this.canauxMessagesService
+      .getCanalById(this.id)
+      .subscribe((data) => (this.canal = data));
   }
-  
 }
